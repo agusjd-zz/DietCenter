24a02164893766e3c485b25bcb0e10bb630c4bdc
Agustín Diaz <agusjd92@gmail.com>
2020-11-19 22:24:57 -0300

    se arreglo secction snack

---
+++ js/tienda.js
@@ -0,0 +1,7 @@
+//Variables
+const cartButtons = document.querySelectorAll('.agregarImg');
+
+cartButtons.forEach((addToCart) => {
+    addToCart.addEventListener('click', () => console.log('click'))
+    
+});
