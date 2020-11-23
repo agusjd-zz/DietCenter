3afb624d191b9776841ebe2c6b88d90e3a53042b
Agustín Diaz <agusjd92@gmail.com>
2020-11-23 16:00:20 -0300

    Se agrego la funcionalidad de agregar items al carrito y actualizar el total

--- js/tienda.js
+++ js/tienda.js
@@ -1,7 +1,72 @@
-//Variables
+/* Agregar Elementos al Carrito */
+
 const cartButtons = document.querySelectorAll('.agregarImg');
+const shopCartContainer = document.querySelector(".shopCartContainer")  
 
 cartButtons.forEach((addToCart) => {
-    addToCart.addEventListener('click', () => console.log('click'))
+    addToCart.addEventListener('click', addToCartClick);
     
 });
+
+function addToCartClick(event){
+    const but = event.target;
+    const item = but.closest(".item");
+    
+    const itemTitle = item.querySelector(".itemTitle").textContent;
+    const itemPrice = item.querySelector(".itemPrice").textContent;
+    const itemImage = item.querySelector(".itemImage").src;
+
+    addItemToCart(itemTitle,itemPrice,itemImage);
+}
+function addItemToCart(itemTitle,itemPrice,itemImage){
+    const shopRow = document.createElement("div");
+    const shopCartContent = `
+    <div class="row shoppingCartItem">
+    <div class="col-6">
+        <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+            <img src=${itemImage} class="shopping-cart-image">
+            <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}</h6>
+        </div>
+    </div>
+    <div class="col-2">
+        <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+            <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
+        </div>
+    </div>
+    <div class="col-4">
+        <div
+            class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+            <input class="shopping-cart-quantity-input shoppingCartItemCant" type="number"
+                value="1">
+            <button class="btn btn-danger buttonDelete" type="button">X</button>
+        </div>
+    </div>
+</div>`;
+
+    shopRow.innerHTML = shopCartContent;
+    shopCartContainer.append(shopRow);
+
+    shopCartTotal()
+   
+}
+
+/*Funcion que actualiza total del carrito*/
+
+function shopCartTotal(){
+    let total = 0;
+    const shopCartTotal = document.querySelector(".shopCartTotal");
+    const shopCartItems = document.querySelectorAll(".shoppingCartItem");
+    
+    shopCartItems.forEach((shoppingCartItem) => {
+        const shopCartItemPriceElement = shoppingCartItem.querySelector(".shoppingCartItemPrice");
+        const shopCartItemPrice = Number(shopCartItemPriceElement.textContent.replace("AR$"," "));
+        const shopCartItemCantElement = shoppingCartItem.querySelector(".shoppingCartItemCant");
+        const shopCartItemCant = Number(shopCartItemCantElement.value);
+        
+        total = total + shopCartItemPrice * shopCartItemCant;
+        console.log(total);
+    });
+    shopCartTotal.innerHTML = `AR$${total}`;
+}
+
+/* 
